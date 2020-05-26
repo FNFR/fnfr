35c0428144f960e44d5c4724b229b85ef8ab2026
Fernando Fonseca <fernando@fnfr.com.br>
2020-05-25 22:16:15 -0300

    favicon

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+                               'favicon.ico', mimetype='image/png')
+                               
 @app.route('/')
 def index():
     return render_template('index.html')
